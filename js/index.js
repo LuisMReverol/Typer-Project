5771bc749bb1fa3366be0888498ee4da4ac17c39
LuisMReverol <allworldinhere@gmail.com>
2020-06-13 00:11:03 -0400

    Added responsive interface, updated new words library based on Bungie's Destiny glossary, added session highscore, title and version number

--- js/index.js
+++ js/index.js
@@ -1,17 +1,67 @@
-var displayWord, displaWordDOM, inputWord, seconds, words, secondsDOM, score, scoreDOM;
+var displayWord, displaWordDOM, inputWord, seconds, words, secondsDOM, score, scoreDOM, highScoreDOM, highScore;
 
 words = [
-  "lucy",
-  "heradocles",
-  "pablo",
-  "armando",
-  "maria",
-  "kazimiro",
-  "asier",
-  "diego",
-  "josean",
-  "unai",
-  "daniel",
+  "cayde-6",
+  "crota",
+  "crucible",
+  "cryptarch",
+  "engram",
+  "exotic",
+  "fireteam",
+  "gjallarhorn",
+  "guardian",
+  "infusion",
+  "major",
+  "nightfall",
+  "oryx",
+  "raid",
+  "lair",
+  "shaxx",
+  "drifter",
+  "hive",
+  "variks",
+  "xur",
+  "vault of glass",
+  "kingsfall",
+  "leviathan",
+  "dark below",
+  "taken king",
+  "ghost",
+  "solar",
+  "arc",
+  "void",
+  "siva",
+  "rasputin",
+  "ana bray",
+  "radegast",
+  "iron lords",
+  "young wolf",
+  "world line zero",
+  "glimmer",
+  "sparrow",
+  "traveler",
+  "piramid",
+  "felwinter",
+  "calus",
+  "awoken",
+  "mara",
+  "uldren",
+  "risen",
+  "light",
+  "dark",
+  "osiris",
+  "dreadnaughts",
+  "golgoroth",
+  "alak-hul",
+  "nokris",
+  "deep stone crypt",
+  "dul incaru",
+  "xyor",
+  "thorn",
+  "savathun",
+  "vex",
+  "fallen",
+  "seed"
 ];
 
 secondsDOM = document.getElementById("time");
@@ -22,7 +72,13 @@ inputWord = document.getElementById("inputField");
 
 displayWordDOM = document.getElementById("word");
 
+highScoreDOM = document.getElementById("high"); 
+
+titleState = document.getElementById("startText");
+
 initial();
+
+highScore = 0;
 
 function timer() {
   var countdown = setInterval(function () {
@@ -31,7 +87,8 @@ function timer() {
     document.getElementById("time").textContent = seconds + "s";
     if (seconds === 0) {
       clearInterval(countdown);
-      initial ();};
+      gameOver();
+    };
   }, 1000);
 }
 
@@ -45,7 +102,7 @@ function randomWord() {
 
 function initial() {
   inputWord.value = "";
-  inputWord.placeholder = "start";
+  inputWord.placeholder = "Type " + "start";
   secondsDOM.textContent = 20;
   seconds = 20;
   score = 0;
@@ -54,6 +111,16 @@ function initial() {
   secondsDOM.textContent = seconds + "s"
 
   inputWord.addEventListener("keyup", gameStart);
+}
+
+function gameOver() {
+  
+  if (score > highScore) {
+  highScore = score;
+  highScoreDOM.textContent = highScore + " Highscore";
+  }
+
+  initial ();
 }
 
 function gameStart() {
@@ -67,11 +134,10 @@ function gameState() {
   timer();
   inputWord.value = "";
   randomWord();
-  inputWord.placeholder = displayWord;
-  
+  inputWord.placeholder = "Type " + displayWord;
   
   inputWord.addEventListener("keyup", () => {
-    console.log(displayWord, inputWord.value)
+    
     if (displayWord === inputWord.value.toLowerCase()) {
       console.log("If verification")
       randomWord();
